6f8367ca9aed9ffb0333a7e278967105567676e7
colddusssh <lunar.developer1@gmail.com>
2025-10-29 19:14:37 +0300

    first commit

---
+++ toBase.py
@@ -0,0 +1,18 @@
+def toNBase(num, base):
+	ans = ""
+	while num:
+		ans += "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"[num % base]
+		num //= base
+	return ans[::-1]
+
+def toDecBase(num, base):
+	num = str(num)
+	ans, lenNum = 0, len(num)
+	for i in range(lenNum):
+		ans += "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".find(num[i]) * base ** (lenNum - 1 - i)
+	return ans
+
+
+n = 9 ** 22 + 3 ** 66 - 12
+
+print(toNBase(n, 3).count("2"))
